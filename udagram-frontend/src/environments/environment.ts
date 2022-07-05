ca5d0b3cd688948e1de001a2b410c46c8b1a0191
HHG UI UX Designer <metail.bird.13@gmail.com>
2022-07-05 23:20:32 +0700

    update api host

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a38f75fd0939444068c90d26aed63410-664023834.us-east-1.elb.amazonaws.com/api/v0' // add end point ( expose Ip)
 };
 
 /*
@@ -14,5 +14,13 @@ export const environment = {
  *
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
+ * 
+ * Run these commands from the /udagram-frontend directory to add new docker image with new apiHost
+ * 
+ * docker build . -t kynkdev/udagram-frontend:v6
+ * docker push kynkdev/udagram-frontend:v6
+ * Run these commands from the /udagram-deployment directory
+ * Rolling update the containers of "frontend" deployment
+   kubectl set image deployment udagram-frontend udagram-frontend=kynkdev/udagram-frontend:v6
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
